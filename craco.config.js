fcf2b33251f79d8b00db9ed3bab8590689e6db86
HaeRyong Jeong <dev.mirr89@gmail.com>
2021-03-22 11:35:42 +0900

    Add craco.config.json and set packages for typescript

---
+++ craco.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const webpack = require('webpack')
+const { whenProd } = require('@craco/craco')
+const { resolve } = require('path')
+
+const isDev = true
+const phase = 'dev'
+const assetPath = '/'
+
+module.exports = function() {
+  return {
+    webpack: {
+      configure: webpackConfig => {
+        return {
+          ...webpackConfig,
+          output: {
+            ...webpackConfig.output,
+            publicPath: '/'
+          }
+        }
+      },
+      alias: {
+        '@': resolve('src'),
+      },
+      devtool: isDev ? 'module-source-map': false,
+      plugins: [
+        new webpack.DefinePlugin({
+          PHASE: JSON.stringify(phase),
+          ASSET_PATH: JSON.stringify(assetPath),
+        }),
+      ],
+    },
+    devServer: devServerConfig => {
+      return {
+        ...devServerConfig,
+      }
+    },
+  }
+}
